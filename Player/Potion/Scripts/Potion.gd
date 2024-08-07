4988e6163dd94c8d5b998f8c4529ee92e931f779
Molflyn <143250086+Molflyn@users.noreply.github.com>
2024-08-07 23:54:23 +0800

    Prep for Potion Fracture

--- Player/Potion/Scripts/Potion.gd
+++ Player/Potion/Scripts/Potion.gd
@@ -1,8 +1,15 @@
 class_name Potion
 extends RigidBody3D
 
+@export var shard_lifetime : float = 5
+
 func _on_body_entered(_body:Node):
-	queue_free()
+	explode()
 
 func explode():
-	pass
+	if $Potion.visible == true and $Potion_Fractured.visible == false:
+		$Potion.queue_free()
+		$Potion_Fractured.visible = true
+
+		await get_tree().create_timer(shard_lifetime).timeout
+		queue_free()
